fa24f0b50a5bfb5d93a287c46a6aeb4901ba874e
Shumck <rmarshall2025@scarboroughschools.org>
2024-12-08 22:25:27 -0500

    Update user.js

--- user.js
+++ user.js
@@ -1,1 +1,7 @@
+var name = document.getElementById("name");
+var age = document.getElementById("age");
+var status = document.getElementById("status");
 
+fetch("user.json")
+  .then(response => response.json())
+  .then(value => console.log(value))
